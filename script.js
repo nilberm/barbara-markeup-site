d6f58890244c1a259fda62926bed655035876238
nilberm <nilber16@outlook.com>
2022-01-05 11:02:57 -0300

    ipad terminado

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ function sendHome(){
     window.location.href = '#home'
 }
 
+var email = document.getElementById('email')
 
+function validationEmail(){
+    
+
+    if(!email.checkValidity()){
+        
+        if(email.classList) email.classList.add('error')
+          
+    }else email.classList.remove("error")
+}
 
